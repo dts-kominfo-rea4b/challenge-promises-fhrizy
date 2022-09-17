73533a883fcec796f878a065923aca901aa19170
fhrizy <mfichar@gmail.com>
2022-09-17 20:33:40 +0700

    make promise then 3 test passed

--- promise.js
+++ promise.js
@@ -1,8 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (input) => {
+   return new Promise((resolve) => {
+      const hasilSetelahMenonton = [];
+      promiseTheaterIXX().then((data) => {
+         hasilSetelahMenonton.push(...data);
+         promiseTheaterVGC().then((data) => {
+            hasilSetelahMenonton.push(...data);
+            if (input === "marah") {
+               const marah = hasilSetelahMenonton.filter(
+                  (item) => item.hasil === "marah"
+               );
+               resolve(marah.length);
+            } else if (input === "tidak marah") {
+               const tidakMarah = hasilSetelahMenonton.filter(
+                  (item) => item.hasil === "tidak marah"
+               );
+               resolve(tidakMarah.length);
+            }
+         });
+      });
+   });
+};
 
 module.exports = {
-  promiseOutput,
+   promiseOutput,
 };
